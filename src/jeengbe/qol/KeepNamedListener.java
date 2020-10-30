4e0a23ac2d8cea6bf2fb2afa252c3cc318868497
jeengbe <jesper.engberg@gmx.at>
2020-10-30 12:49:46 +0100

    KeepNamed

---
+++ src/jeengbe/qol/KeepNamedListener.java
@@ -0,0 +1,35 @@
+package jeengbe.qol;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+/**
+ * Named items do not despawn natuarlly on death
+ */
+public class KeepNamedListener implements Listener {
+  @EventHandler
+  public void onDeath(PlayerDeathEvent e) {
+    List<ItemStack> live = new ArrayList<>();
+    for (ItemStack item : e.getDrops()) {
+      if (item.hasItemMeta()) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta.hasDisplayName()) {
+          live.add(item);
+        }
+      }
+    }
+    e.getDrops().removeAll(live);
+
+    for (ItemStack item : live) {
+      Item i = e.getEntity().getLocation().getWorld().dropItemNaturally(e.getEntity().getLocation(), item);
+      i.setTicksLived(-32768);
+    }
+  }
+}
--- src/jeengbe/qol/Main.java
+++ src/jeengbe/qol/Main.java
@@ -13,5 +13,6 @@ public class Main extends JavaPlugin implements Listener {
     rand = new Random();
     getServer().getPluginManager().registerEvents(this, this);
     getServer().getPluginManager().registerEvents(new CreeperChainListener(), this);
+    getServer().getPluginManager().registerEvents(new KeepNamedListener(), this);
   }
 }
