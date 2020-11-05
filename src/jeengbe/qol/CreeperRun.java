09d07dc9beb326fbdd068170da8c7bb5cec64043
jeengbe <jesper.engberg@gmx.at>
2020-11-05 14:57:12 +0100

    CreeperRun Description Typo

--- src/jeengbe/qol/CreeperRun.java
+++ src/jeengbe/qol/CreeperRun.java
@@ -14,7 +14,7 @@ import net.minecraft.server.v1_16_R2.EntityCreature;
 import net.minecraft.server.v1_16_R2.GenericAttributes;
 
 /**
- * Mobs run away from exploding creepers
+ * Mobs run away from exploding Creepers
  */
 public class CreeperRun implements Listener {
   @EventHandler
